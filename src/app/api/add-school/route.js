896e2a5e1d96a529369d11c4948c87fecaf7bb19
Tushar Mishra <tusharmishra9695@gmail.com>
2024-02-03 14:25:59 +0530

    #05 db connected and get api fetch data from mysql

--- src/app/api/add-school/route.js
+++ src/app/api/add-school/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
+import { db } from "@/app/lib/db";
 export async function POST(req, res) {
   let payload = await req.json();
+
   if (
     !payload.name ||
     !payload.address ||
@@ -13,9 +15,37 @@ export async function POST(req, res) {
       { result: "require field not found", success: false },
       { status: 400 }
     );
+  } else {
+    var sql =
+      "INSERT INTO dummy VALUES ('" +
+      payload.name +
+      "','" +
+      payload.address +
+      "','" +
+      payload.city +
+      "','" +
+      payload.state +
+      "','" +
+      payload.contact +
+      "','" +
+      payload.image +
+      "','" +
+      payload.email +
+      "')";
+    try {
+      db.query(sql, (err, results) => {
+        if (err) {
+          console.log("db not connected");
+        } else {
+          console.log("registered");
+        }
+      });
+      // return NextResponse.json(
+      //   { result: "School registered!", success: true },
+      //   { status: 201 }
+      // );
+    } catch (e) {
+      return NextResponse.json({ message: e, success: false }, { status: 500 });
+    }
   }
-  return NextResponse.json(
-    { result: "new user created", success: true },
-    { status: 201 }
-  );
 }
--- src/app/api/show-school/route.js
+++ src/app/api/show-school/route.js
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
+import { db } from "@/app/lib/db";
 export async function GET(req) {
-  return NextResponse.json(
-    { result: "school data", success: true },
-    { status: 201 }
-  );
+  try {
+    const list = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM school_list", (err, results) => {
+        if (err) {
+          console.log("db not connected");
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    return NextResponse.json({ result: list, success: true }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ message: e, success: false }, { status: 500 });
+  }
 }
