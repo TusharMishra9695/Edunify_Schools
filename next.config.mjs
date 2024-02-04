8630953801ef8dea0236dd718aa95d0e24f1c0e1
Tushar Mishra <tusharmishra9695@gmail.com>
2024-02-04 15:02:10 +0530

    # cors post api error fix try 14 prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
