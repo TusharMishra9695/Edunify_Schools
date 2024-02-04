c909c05da2bb16614d6065d6235fe93afd62f624
Tushar Mishra <tusharmishra9695@gmail.com>
2024-02-04 15:07:19 +0530

    # cors post api error fix try 16 prod

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://edunify-schools.vercel.app",
-          },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
@@ -19,7 +16,7 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
